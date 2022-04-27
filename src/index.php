afdfad87d451b262372d4d353a32b507bffd9fff
Aman Tiwari <amantiwari@cedcommerce.com>
2022-04-27 14:48:37 +0530

    updated

--- src/index.php
+++ src/index.php
@@ -7,9 +7,16 @@
 	<link href="style.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-	<?php
-	include "header.php";
-	?>
+<div id="header">
+		<h1 id="logo">Logo</h1>
+		<nav>
+			<ul id="menu">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="products.php">Products</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</nav>
+	</div>
 	
 	<div id="main">
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
@@ -18,8 +25,13 @@
 		<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
 		<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
 	</div>
-	<?php
-	include "footer.php";
-	?>
+	<div id="footer">
+		<nav>
+			<ul id="footer-links">
+				<li><a href="#">Privacy</a></li>
+				<li><a href="#">Declaimers</a></li>
+			</ul>
+		</nav>
+	</div>
 </body>
 </html>
